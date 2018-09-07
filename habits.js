38117c31630afc694a3ab1659e65a25e7a34c5e2
kdthanvi <thanvikaran95@gmail.com>
2018-09-08 02:25:36 +0900

    add custom habits & reasons

--- habits.js
+++ habits.js
@@ -3,10 +3,12 @@
 module.exports = {
 
     data : [
-        { habit : 'habit 1', reason : 'reason 1' },
-        { habit : 'habit 2', reason : 'reason 2' },
-        { habit : 'habit 3', reason : 'reason 3' },
-        { habit : 'habit 4', reason : 'reason 4' },
-        { habit : 'habit 5', reason : 'reason 5' }
+        { habit : 'Brush your teeth regularly in the morning and before going to sleep.', reason : 'reason 1' },
+        { habit : 'Follow bedtiem routine.', reason : 'Minimum 8 hours of sleep is essential  for children . You should be in bed latest by 10 PM. Early to bed early to rise makes a man healthy, wealthy and wise' },
+        { habit : 'Drink plenty of water.', reason : 'Water helps body in multiple ways. It carries nutrients and most importantly it helps to remove waste products from out body. It also helps to control body temparature. Less water consumption can cause dehydration.' },
+        { habit : 'Breakfast is the most important meal of the day.', reason : 'It is the most important meal of the day and it gives maximum energy. A healthy breakfase contains a portion of fruits and a glass of milk with oats and honey, dosa, and chutney or bread and marmalade.' },
+        { habit : 'Give respect to elders.', reason : 'reason X' },
+        { habit : 'Do not spend more than 1 hour on watching TV, playing on XBOX or iPad.', reason : 'Watching TV, playing on XBOX or iPad can damage your eyesight. Watching TV for long time can make you dumb. It also adversely affect health.'},
+        { habit : 'Play outdoor games', reason : 'Playing outdoor games is necessary for healthy body. It helps in increasing physical stamina and fitness. Playing outdoor games also teaches us important life lessons like team work, problem solving skills, decision making. It helps to gain self confidence, creativity, develop muscular strength and coordination.'}
     ]
 };
