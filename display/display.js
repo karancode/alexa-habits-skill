b1835b21c28bcd88559edd18c061cf27d3be0fe6
karancode <karanthanvi0@gmail.com>
2018-09-10 15:24:00 +0900

    add getdisplay()

--- display/display.js
+++ display/display.js
@@ -6,9 +6,61 @@ module.exports = {
             handlerInput.requestEnvelope.context &&
             handlerInput.requestEnvelope.context.System &&
             handlerInput.requestEnvelope.context.System.device &&
-            handlerInput.requestEnvelope.context.System.device.supportedInterface &&
-            handlerInput.requestEnvelope.context.System.device.supportedInterface.Display;
+            handlerInput.requestEnvelope.context.System.device.supportedInterfaces &&
+            handlerInput.requestEnvelope.context.System.device.supportedInterfaces.Display;
         
         return hasDisplay;
-    } 
+    },
+    getDisplay : (response, display_type, display_habbit, display_reason) => {
+        //const image = new Alexa.ImageHelper().addImageInstance(image_url).getImage();
+          
+        const myLaunchTextContent = new Alexa.RichTextContentHelper()
+            .withPrimaryText('Welcome to HABITS skill!' + "</br>")
+            .withSecondaryText('You can ask me for habits and I will teach you good!')
+            .withTertiaryText('You can say, \'Tell me a habit...\'')
+            .getTextContent();
+        
+        const myHabitTextContent = new Alexa.RichTextContentHelper()
+            .withPrimaryText('Habit : ' + display_habbit + '</br>')
+            .withSecondaryText('Reason : ' + display_reason + '</br>')
+            .getTextContent();
+        
+        const myReasonTextContent = new Alexa.RichTextContentHelper()
+            .withPrimaryText('Habit : ' + display_habbit + '</br>')
+            .withSecondaryText('Reason : ' + display_reason + '</br>')
+            .getTextContent();
+
+
+        if(display_habbit === null && display_reason === null)
+        {
+            response.addRenderTemplateDirective({
+                type : display_type,
+                backButton : 'invisible',
+                //backgroundImage : image,
+                title : 'HABIT Skill',
+                textContent : myLaunchTextContent
+            });
+        }
+        else if(display_habbit != null && display_reason === null)
+        {
+            response.addRenderTemplateDirective({
+                type : display_type,
+                backButton : 'visible',
+                //backgroundImage : image,
+                title : 'Habit - HABIT Skill',
+                textContent : myHabitTextContent
+            });
+        }
+        else if(display_habbit === null && display_reason != null)
+        {
+            response.addRenderTemplateDirective({
+                type : display_type,
+                backButton : 'visible',
+                //backgroundImage : image,
+                title : 'Reason - HABIT Skill',
+                textContent : myReasonTextContent
+            });
+        }
+        return response;
+    }
 };
